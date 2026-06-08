c43e5ad89d195a18a2db318331ad8b208f5204a2
Enrica Schmidt <enrica.schmidt@aol.de>
2026-06-08 14:50:13 +0200

    makefile and sizes merge

--- verilog/dv/wb_test_icesoc_copy/program_and_bitstream/instr_bitstr_sizes.h
+++ verilog/dv/wb_test_icesoc_copy/program_and_bitstream/instr_bitstr_sizes.h
@@ -4,7 +4,7 @@
 #include "memory.h"
 #include "bitstream.h"
 
-#define BITSTREAM_WORDS 32//4506 //full bitstream 4506 words
+#define BITSTREAM_WORDS 2406//4506 //full bitstream 4506 words
 #define INSTRUCTION_WORDS 80 //nr of instructions to be written to sram (not including page A-C) (PAGE_SIZE_INSTRS/4 * nr of virtual pages)
 
 #define BITSTR_WORDS_PER_PAGE 16 //nr words per page, only 1/4th is addressable
