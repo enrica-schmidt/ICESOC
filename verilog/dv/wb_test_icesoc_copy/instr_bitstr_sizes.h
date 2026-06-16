b387033b27fa8d31925ceca721eb8abfc7b498be
Enrica Schmidt <enrica.schmidt@aol.com>
2026-06-16 13:47:04 +0200

    edited Makefile for multiple simulation runs in parallel. create one folder for every run. copy source files there

--- verilog/dv/wb_test_icesoc_copy/instr_bitstr_sizes.h
+++ verilog/dv/wb_test_icesoc_copy/instr_bitstr_sizes.h
@@ -11,3 +11,14 @@
 #define INSTRS_WORDS_PER_PAGE 16 //nr words per page, only 1/4th is addressable
 
 #endif /* INSTR_BITSTR_SIZES_H */
+
+/*
+#include "programs/program_a.h"            //5 deadbeef pages
+#include "bitstreams/bitstream_shrunk_a.h" //counter on shrunk fabric 
+
+#define BITSTREAM_WORDS 2406 //full bitstream 4506 words //2406 shrunk
+#define INSTRUCTION_WORDS 80 //nr of instructions to be written to sram (not including page A-C) (PAGE_SIZE_INSTRS/4 * nr of virtual pages)
+
+#define BITSTR_WORDS_PER_PAGE 16 //nr words per page, only 1/4th is addressable
+#define INSTRS_WORDS_PER_PAGE 16 //nr words per page, only 1/4th is addressable
+*/
